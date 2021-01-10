1a864e1546407bc6dbb342d1453d868fea025485
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2021-01-10 09:14:37 -0600

    update create logos

--- src/main/webapp/app/entities/plataforma/plataforma.vue
+++ src/main/webapp/app/entities/plataforma/plataforma.vue
@@ -25,74 +25,52 @@
     <div class="alert alert-warning" v-if="!isFetching && plataformas && plataformas.length === 0">
       <span v-text="$t('crewApp.plataforma.home.notFound')">No plataformas found</span>
     </div>
-    <div class="table-responsive" v-if="plataformas && plataformas.length > 0">
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th v-on:click="changeOrder('id')">
-              <span v-text="$t('global.field.id')">ID</span>
-              <jhi-sort-indicator :current-order="propOrder" :reverse="reverse" :field-name="'id'"></jhi-sort-indicator>
-            </th>
-            <th v-on:click="changeOrder('nombre')">
-              <span v-text="$t('crewApp.plataforma.nombre')">Nombre</span>
-              <jhi-sort-indicator :current-order="propOrder" :reverse="reverse" :field-name="'nombre'"></jhi-sort-indicator>
-            </th>
-            <th v-on:click="changeOrder('color')">
-              <span v-text="$t('crewApp.plataforma.color')">Color</span>
-              <jhi-sort-indicator :current-order="propOrder" :reverse="reverse" :field-name="'color'"></jhi-sort-indicator>
-            </th>
-            <th v-on:click="changeOrder('logo')">
-              <span v-text="$t('crewApp.plataforma.logo')">Logo</span>
-              <jhi-sort-indicator :current-order="propOrder" :reverse="reverse" :field-name="'logo'"></jhi-sort-indicator>
-            </th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="plataforma in plataformas" :key="plataforma.id">
-            <td>
-              <router-link :to="{ name: 'PlataformaView', params: { plataformaId: plataforma.id } }">{{ plataforma.id }}</router-link>
-            </td>
-            <td>{{ plataforma.nombre }}</td>
-            <td><input type="color" disabled :value="plataforma.color" /></td>
-            <td>
-              <a v-if="plataforma.logo" v-on:click="openFile(plataforma.logoContentType, plataforma.logo)">
-                <img
-                  v-bind:src="'data:' + plataforma.logoContentType + ';base64,' + plataforma.logo"
-                  style="max-height: 30px"
-                  alt="plataforma image"
-                />
-              </a>
-              <span v-if="plataforma.logo">{{ plataforma.logoContentType }}, {{ byteSize(plataforma.logo) }}</span>
-            </td>
-            <td class="text-right">
-              <div class="btn-group">
-                <router-link
-                  :to="{ name: 'PlataformaView', params: { plataformaId: plataforma.id } }"
-                  tag="button"
-                  class="btn btn-info btn-sm details"
-                >
-                  <font-awesome-icon icon="eye"></font-awesome-icon>
-                  <span class="d-none d-md-inline" v-text="$t('entity.action.view')">View</span>
-                </router-link>
-                <router-link
-                  :to="{ name: 'PlataformaEdit', params: { plataformaId: plataforma.id } }"
-                  tag="button"
-                  class="btn btn-primary btn-sm edit"
-                >
-                  <font-awesome-icon icon="pencil-alt"></font-awesome-icon>
-                  <span class="d-none d-md-inline" v-text="$t('entity.action.edit')">Edit</span>
-                </router-link>
-                <b-button v-on:click="prepareRemove(plataforma)" variant="danger" class="btn btn-sm" v-b-modal.removeEntity>
-                  <font-awesome-icon icon="times"></font-awesome-icon>
-                  <span class="d-none d-md-inline" v-text="$t('entity.action.delete')">Delete</span>
-                </b-button>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+
+    <b-table
+      :striped="false"
+      :borderless="true"
+      :no-border-collapse="false"
+      :busy="isFetching"
+      :small="true"
+      :hover="false"
+      :fixed="false"
+      head-variant="light"
+      :items="plataformas"
+      :fields="['color', 'nombre', 'acciones']"
+    >
+      <template #table-busy>
+        <div class="text-center text-danger my-2">
+          <b-spinner class="align-middle"></b-spinner>
+          <strong>Cargando...</strong>
+        </div>
+      </template>
+
+      <template #cell(color)="data">
+        <b-avatar :text="data.item.nombre | formatAvatarText" :style="{ 'background-color': data.item.color }" size="sm"></b-avatar>
+      </template>
+
+      <template #cell(acciones)="row">
+        <div class="btn-group">
+          <router-link
+            :to="{ name: 'PlataformaView', params: { plataformaId: row.item.id } }"
+            tag="button"
+            class="btn btn-outline-info btn-sm details"
+          >
+            <font-awesome-icon icon="eye"></font-awesome-icon>
+          </router-link>
+          <router-link
+            :to="{ name: 'PlataformaEdit', params: { plataformaId: row.item.id } }"
+            tag="button"
+            class="btn btn-outline-primary btn-sm edit"
+          >
+            <font-awesome-icon icon="pencil-alt"></font-awesome-icon>
+          </router-link>
+          <b-button v-on:click="prepareRemove(row.item)" variant="outline-danger" class="btn btn-sm" v-b-modal.removeEntity>
+            <font-awesome-icon icon="times"></font-awesome-icon>
+          </b-button>
+        </div>
+      </template>
+    </b-table>
     <b-modal ref="removeEntity" id="removeEntity">
       <span slot="modal-title"
         ><span id="crewApp.plataforma.delete.question" v-text="$t('entity.delete.title')">Confirm delete operation</span></span
